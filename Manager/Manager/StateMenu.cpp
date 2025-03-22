4649c891efbb21c3f726b289c0014f8f74afafc2
Ostropode <150232303+Ostropode@users.noreply.github.com>
2025-03-22 14:39:53 +0100

    T

--- Manager/Manager/StateMenu.cpp
+++ Manager/Manager/StateMenu.cpp
@@ -32,7 +32,7 @@ void StateMenu::Update()
 	if (MainMenu.GetList().size() == 0)
 	{
 		MainMenu.Add(new Button(TO_GAME, sf::Vector2f(150.f, 75.f),  MainMenu));
-		MainMenu.Add(new Button(TO_MENU, sf::Vector2f(150.f, 275.f), MainMenu));
+		MainMenu.Add(new Button(TO_SETTINGS, sf::Vector2f(150.f, 275.f), MainMenu));
 		MainMenu.Add(new Button(TO_QUIT, sf::Vector2f(150.f, 475.f), MainMenu));
 	}
 
